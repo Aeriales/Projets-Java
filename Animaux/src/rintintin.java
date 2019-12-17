48ac88d4666ce40a00bdc0ea0885956cdcbdf065
Raphael <58846626+Aeriales@users.noreply.github.com>
2019-12-17 17:04:35 +0100

    Add files via upload

---
+++ Animaux/src/rintintin.java
@@ -0,0 +1,5 @@
+public interface rintintin {
+    public void fairelebeau();
+    public void fairecalin();
+    public void fairelechouille();
+}
---
+++ Animaux/src/tigre.java
@@ -0,0 +1,15 @@
+public class tigre extends felin {
+
+    public tigre(){
+
+    }
+
+    public tigre(String couleur, int poids){
+        this.couleur=couleur;
+        this.poids=poids;
+    }
+
+    void crier(){
+        System.out.println("Je grogne très fort");
+    }
+}
